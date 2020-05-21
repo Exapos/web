6b49e9467cf1287f7f389aa70813abfead3b53c6
Jakub Záruba <info@jakub-zaruba.cz>
2020-05-21 21:14:11 +0200

    WIP login

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
 require "../vendor/autoload.php";
 
 use Tracy\Debugger;
+
 Debugger::enable(Debugger::DEVELOPMENT);
 Debugger::$showBar = true;
 
@@ -13,4 +16,18 @@ Debugger::$showBar = true;
 require "../includes/config.local.php";
 
 $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
-        
+
+if (isset($_SESSION['cookie'])) { // jsem přihlášený
+
+    $cookie = $_SESSION['cookie'];
+    $sql = "SELECT * FROM register WHERE cookie = '$cookie'";
+
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        $user = $row; // v proměnné "row" je přihlášený uživatel
+        break;
+    }
+    
+} else { // nejsem příhlášený, přesměruju ho na login
+    header('Location: /login/index.html');
+}
--- login/login.php
+++ login/login.php
@@ -2,29 +2,26 @@
 
 require('../bootstrap.php');
 
-$username = "Exapos";//$_POST['username'];
-$password =  "123";//$_POST['password'];
-
+$username = "Exapos"; //$_POST['username'];
+$password =  "test"; //$_POST['password'];
 
 if (!empty($username) && !empty($password)) {
-    
-    $sql = "SELECT * FROM register WHERE username = '$username'";
 
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-     
-      while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. $row["username"]. "<br>";
-      }
-    } else {
-      echo "0 results";
-    } 
+  $password = sha1($password); // před hledáním, heslo zahashuju
+  $sql = "SELECT * FROM register WHERE username = '$username' AND password = '$password'";
 
-    $conn->close();
-    
-    
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+      // údaje jsou správně
+      $_SESSION['cookie'] = $cookie = $_COOKIE["PHPSESSID"];
+      $sql = "UPDATE register SET cookie = '$cookie' WHERE username = '$username' AND password = '$password'";
+      $conn->query($sql);
+     }
+  } else {
+    echo "Invalid credentials";
+  }
+
+  $conn->close();
 }
-bdump($conn);
-bdump($sql);
-bdump($stmt);
