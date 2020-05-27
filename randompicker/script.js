42edb241b08b58377150bdba44130f74631ebd21
Exapos <klamota@email.cz>
2020-05-27 21:31:37 +0200

    IMG and GRID randompick

--- randompicker/script.js
+++ randompicker/script.js
@@ -1,28 +1,51 @@
 $(document).ready(function () {
 
     var maps = [
-        {'name':'Inferno'},
-        {'name':'Train'},
-        {'name':'Mirage'},
-        {'name':'Nuke'},
-        {'name':'Overpass'},
-        {'name':'Dust_II'},
-        {'name':'Vertigo'}
+        {'name':'Inferno','img':'inferno.png'},
+        {'name':'Train','img':'train.png'},
+        {'name':'Mirage','img':'mirage.png'},
+        {'name':'Nuke','img':'nuke.png'},
+        {'name':'Overpass','img':'overpass.png'},
+        {'name':'Dust_II','img':'dust2.png'},
+        {'name':'Vertigo','img':'vertigo.png'},
+        {'name':'Office','img':'office.png'}
     ];
 
     var pickedMaps = [
 
     ];
 
-    var mapsContent = "";
-    var index = 0;
-    maps.forEach(element => {
-        mapsContent += '<label for="'+index+'"><input type="checkbox" class="checkbox-map" id="'+index+'">'+element.name+'<br></label>'+
-                       '<br>';
-        index++;
-    });
 
-    $('#maps').html(mapsContent);
+    var mapsContent = '';
+
+    var index = 1;
+    maps.forEach(element => {
+        if(index == 1){
+            mapsContent+= '<div class="d-flex">';
+        }
+
+        var elementImg = '<img src="img/'+element.img+'" class="map-img">';
+        mapsContent += '<div class="flex-fill"><label for="'+index+'"><input type="checkbox" class="checkbox-map" id="'+index+'">'+elementImg +'<br></label>'+
+                       '</div>';
+
+        if(index % 4 == 0){ //pokud je zbytek po dělení == 0, "modulo"
+            // nový řádek
+            mapsContent+= '</div>';
+            mapsContent+= ' <div class="d-flex">';
+        }
+        
+        
+        if(index == maps.length){
+            console.log(index);
+            console.log(maps.length);
+            mapsContent+= '</div>';
+        }
+        index++;
+
+       
+    });
+    mapsContent += '</div>';
+    $('#maps').append(mapsContent);
 
     $("body").on( "click", ".checkbox-map", function() {
       
