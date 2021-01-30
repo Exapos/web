a97c400139003db8f561fa590498c5cea37a4a90
Exapos <klamota@email.cz>
2021-01-30 13:38:30 +0100

    Game done
    Final changes

--- games/agar/script.js
+++ games/agar/script.js
@@ -1,10 +1,15 @@
 const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const c = canvas.getContext("2d");
 
+
+const scoreEl = document.querySelector('#scoreEl')
+const startBtn = document.querySelector('#gameStartBtn')
+const modalEl = document.querySelector('#modalEl')
+const bigScoreEl= document.querySelector('#bigScoreEl')
 class Player {
   constructor(x, y, radius, color) {
     this.x = x;
@@ -67,6 +72,7 @@ class Enemy {
   }
 }
 
+const friction = 0.99
 class Particle {
     constructor(x, y, radius, color, velocity) {
       this.x = x;
@@ -78,26 +84,42 @@ class Particle {
     }
   
     draw() {
+      c.save()  
+      c.globalAlpha = this.alpha
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
       c.fillStyle = this.color;
       c.fill();
+      c.restore()
     }
   
     update() {
       this.draw();
+      this.velocity.x *= friction
+      this.velocity.y *= friction
       this.x = this.x + this.velocity.x;
       this.y = this.y + this.velocity.y;
+      this.alpha -= 0.01
     }
   }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 10, "white");
-const projectiles = []
-const enemies = []
-const particles = []
+let player = new Player(x, y, 10, "white");
+let projectiles = []
+let enemies = []
+let particles = []
+
+function init() {
+     player = new Player(x, y, 10, "white");
+     projectiles = []
+     enemies = []
+     particles = []
+     score = 0
+     scoreEl.innerHTML = score
+     bigScoreEl.innerHTML = score
+}
 
 function spawnEnemies() {
   setInterval(() => {
@@ -123,15 +145,37 @@ function spawnEnemies() {
   }, 1000);
 }
 
-let animationId;
+let animationId
+let score = 0
 function animate() {
   animationId = requestAnimationFrame(animate);
   c.fillStyle = "rgba(0, 0, 0, 0.1)";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  particles.forEach(particle => {
-      particle.update()
+
+
+
+
+
+
+
+  ////////////////////PARTICLE//////////////////
+  particles.forEach((particle, index) => {
+      if (particle.alpha <= 0) {
+          particles.splice(index, 1)
+      } else {     
+         particle.update()
+      }
+
   })
+
+
+
+
+
+
+
+////////////////////PROJECTILE//////////////////
   projectiles.forEach((projectile, index) => {
     projectile.update();
 
@@ -155,6 +199,8 @@ function animate() {
     ////////end game//////
     if (dist - enemy.radius - player.radius < 1) {
       cancelAnimationFrame(animationId);
+      modalEl.style.display = 'flex'
+      bigScoreEl.innerHTML = score
     }
 
     projectiles.forEach((projectile, projectileIndex) => {
@@ -163,14 +209,21 @@ function animate() {
       ////////////////when projectiles touch enemy///////////////
 
       if (dist - enemy.radius - projectile.radius < 1) {
-
-        for (let i = 0; i < 8; i++) {
-            particles.push(new Particle(projectile.x, projectile.y, 3, enemy.color, {x: Math.random() - 0.5, y: Math.random() - 0.5}))
+            ///////////////create explosion/////////////
+        for (let i = 0; i < enemy.radius * 2; i++) {
+            particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, 
+            {x: (Math.random() - 0.5) * (Math.random() * 8), 
+             y: (Math.random() - 0.5) * (Math.random() * 8)
+            }))
             
         }
 
         if (enemy.radius - 10 > 5) {
-          gsap.to(enemy, {
+            ////////increase our score////////
+            score += 100
+            scoreEl.innerHTML = score
+
+            gsap.to(enemy, {
               radius: enemy.radius -10
           })
           setTimeout(() => {
@@ -178,6 +231,10 @@ function animate() {
           }, 0)
 
         } else {
+            ///remove from scene
+            ////////increase our score////////
+            score += 250
+            scoreEl.innerHTML = score
           setTimeout(() => {
             enemies.splice(index, 1);
             projectiles.splice(projectileIndex, 1);
@@ -187,6 +244,7 @@ function animate() {
     })
   })
 }
+
 
 addEventListener("click", (event) => {
   const angle = Math.atan2(
@@ -204,5 +262,9 @@ addEventListener("click", (event) => {
   );
 });
 
-animate();
-spawnEnemies();
+startGameBtn.addEventListener('click', () => {
+    init()
+    animate()
+    spawnEnemies()
+    modalEl.style.display = 'none'
+})
